954e62bfbada2151de77901df86b672131b3e239
raxavelo <raxavelo@gmail.com>
2024-11-07 21:59:58 +0100

    Updated task 02

--- goit-qaa-hw-7/js/task-02.js
+++ goit-qaa-hw-7/js/task-02.js
@@ -8,9 +8,12 @@ const ingredients = [
 ];
 
 const ingredientsList = document.getElementById("ingredients");
+const fragment = document.createDocumentFragment();
 ingredients.forEach((ingredient) => {
   const item = document.createElement("li");
   item.textContent = ingredient;
   item.classList.add("item");
-  ingredientsList.appendChild(item);
+  fragment.appendChild(item);
 });
+
+ingredientsList.appendChild(fragment);
